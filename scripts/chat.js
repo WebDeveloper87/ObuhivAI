e45e74586084158c58e2397d3bfcd0607f0fd714
Шенько Маргарита <margaritashenko@gmail.com>
2025-12-15 15:52:20 +0200

    відправка повідомлень

--- scripts/chat.js
+++ scripts/chat.js
@@ -1,10 +1,29 @@
 const send = document.getElementsByClassName('send')[0]
 const input = document.getElementsByClassName('input')[0]
+const content = document.getElementsByClassName('content')[0]
 
 send.addEventListener('click', () => {
     if(!input.value){
         return
     }
-    console.log(input.value)
-    input.value = ''
+
+    content.innerHTML +=`
+         <div class="message">
+            <p>${input.value}</p>
+         </div>`
+             input.value = ''
+})
+
+document.addEventListener('keydown', (e)=> { 
+    if (e.key.toLowerCase() === 'enter') {
+        if(!input.value){
+        return
+    }
+
+    content.innerHTML +=`
+         <div class="message">
+            <p>${input.value}</p>
+         </div>`
+             input.value = ''
+    }
 })
